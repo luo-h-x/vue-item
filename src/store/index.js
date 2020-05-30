c24bef59c07c3eca398d668a309c53ea92e14421
luo-h-x <1877430627@qq.com>
2020-05-30 22:06:59 +0800

    store

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,33 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    username: '',
+    password: '',
+    show: true,
+    ismobile: false
+  },
+  getters: {
+    username (state) {
+      return state.username || sessionStorage.getItem('loginusername')
+    }
   },
   mutations: {
+    setLogin (state, val) {
+      state.username = val
+      sessionStorage.setItem('loginusername', val)
+    },
+    close (state) {
+      state.show = false
+    },
+    open (state) {
+      state.show = true
+    },
+    isMobile (state) {
+      state.ismobile = true
+    },
+    removeMobile (state) {
+      state.ismobile = false
+    }
   },
   actions: {
   },
