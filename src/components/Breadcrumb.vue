cfe9510b4423ad74072945fbb53078853bab5a9a
luo-h-x <1877430627@qq.com>
2020-06-04 10:09:40 +0800

    breadcrumb

---
+++ src/components/Breadcrumb.vue
@@ -0,0 +1,46 @@
+<template>
+  <el-breadcrumb separator="/">
+    <transition-group name="breadcrumb">
+      <el-breadcrumb-item v-for="(item) in levelList" :key="item.path">
+        <span  class="no-redirect">{{ item.meta.title }}</span>
+      </el-breadcrumb-item>
+    </transition-group>
+  </el-breadcrumb>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      levelList: null
+    }
+  },
+  watch: {
+    $route () {
+      this.getBreadcrumb()
+    }
+  },
+  created () {
+    this.getBreadcrumb()
+  },
+  methods: {
+    getBreadcrumb () {
+      this.levelList = this.$route.matched.filter(item => item.meta && item.meta.title)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.el-breadcrumb {
+    display: inline-block;
+    font-size: 16px;
+    line-height: 60px;
+    margin-left: 8px;
+
+    .no-redirect {
+      color: #97a8be;
+      cursor: text;
+    }
+  }
+</style>
