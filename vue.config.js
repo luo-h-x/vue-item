c5727f692f3afe8efb509ae1f6069a1fa6acd4db
luo-h-x <1877430627@qq.com>
2020-06-12 11:28:13 +0800

    vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,24 @@
 module.exports = {
   devServer: {
-    proxy: 'http://news-at.zhihu.com/api/4'
-  }
+    // proxy: 'http://news-at.zhihu.com/api/4'
+    proxy: {
+      '/dy': {
+        target: 'https://www.iesdouyin.com/web/api/v2',
+        ws: true,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/dy': ''
+        }
+      },
+      '/news': {
+        target: 'http://news-at.zhihu.com/api/4/news',
+        ws: true,
+        changeOrigin: true,
+        pathRewrite: {
+          '^/news': ''
+        }
+      }
+    }
+  },
+  publicPath: './'
 }
